0b61c6ce1233dd608a06634c7e54238982e86b1d
IamWilliamWang <iamjerichoholic@hotmail.com>
2017-01-12 17:47:33 +0800

    Windows Processes Manager

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcessManager
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        CollectionViewSource view = new CollectionViewSource();
+        ObservableCollection<Process> processesCollection = new ObservableCollection<Process>();
+        System.Diagnostics.Process[] processes;
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread dialogThread = new Thread(new ThreadStart(ThreadProc));
+            dialogThread.Start();
+
+            processes = System.Diagnostics.Process.GetProcesses();
+            this.textBlock1.Text = "进程任务管理器，这里显示所有的系统进程，共" + processes.Length + "个进程";
+            
+            processesCollection.Clear();
+            
+            foreach (System.Diagnostics.Process eachProcess in processes)
+            {
+                
+                processesCollection.Add(new Process(eachProcess));
+                
+            }
+
+            //processesCollection = new ObservableCollection<MyProcess>(processesCollection.OrderBy(item => item.进程名));
+
+            view.Source = processesCollection;
+
+            this.listView1.DataContext = view;
+            
+        }
+
+        private void ThreadProc()
+        {
+            MessageBox.Show("程序正在响应,请等待3-10秒！", "进程管理器");
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Loaded(sender, e);
+        }
+
+        private void btnKill_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("如果某个打开的程序与此进程关联，则会关闭此程序而且将丢失所有未保存的数据。如果结束某个系统进程，则可能导致系统不稳定。你确定要继续吗？", "你希望结束" + processesCollection[listView1.SelectedIndex].进程名 + ".exe吗？",MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                return;
+            try
+            {
+                processes[listView1.SelectedIndex].Kill();
+            }
+            catch
+            {
+                MessageBox.Show("结束进程失败", "错误");
+            }
+        }
+
+        private void btnNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+
+        //强制结束新线程
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ Process.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManager
+{
+    class Process
+    {
+        public int ID { get; set; }
+        public String 进程名 { get; set; }
+        public DateTime? 启动时间 { get; set; }
+        public long? 获得内存大小 { get; set; }
+        public long? 获得虚存大小 { get; set; }
+        public System.Diagnostics.ProcessPriorityClass? 优先级 { get; set; }
+        public int? 优先级参数 { get; set; }
+        public IntPtr? 句柄 { get; set; }
+        public int? 句柄数 { get; set; }
+        public bool? 被终止 { get; set; }
+        public String 运行机器名称 { get; set; }
+        public String 窗口标题 { get; set; }
+        public IntPtr? 最大工作集大小 { get; set; }
+        public IntPtr? 最小工作集大小 { get; set; }
+        public bool? 拥有抢占权限 { get; set; }
+        public bool? 正在响应 { get; set; }
+        public TimeSpan? 处理机执行时长 { get; set; }
+        public TimeSpan? 获取处理机时长 { get; set; }
+        public TimeSpan? 处理机获取和执行时长 { get; set; }
+
+        /*初始化各值，如果权限不足以获取属性，则值其为空然后继续遍历*/
+        public Process(System.Diagnostics.Process eachProcess)
+        {
+            ID = eachProcess.Id;
+            try { 优先级参数 = eachProcess.BasePriority; } catch { }
+            try { 优先级 = eachProcess.PriorityClass; } catch { }
+            try { 句柄 = eachProcess.Handle; } catch { }
+            try { 句柄数 = eachProcess.HandleCount; } catch { }
+            //try { 被终止 = eachProcess.HasExited; } catch { }
+            //try { 运行机器名称 = eachProcess.MachineName; } catch { }
+            try { 窗口标题 = eachProcess.MainWindowTitle; } catch { }
+            try { 最大工作集大小 = eachProcess.MaxWorkingSet; } catch { }
+            try { 最小工作集大小 = eachProcess.MinWorkingSet; } catch { }
+            try { 拥有抢占权限 = eachProcess.PriorityBoostEnabled; } catch { }
+            try { 处理机执行时长 = eachProcess.PrivilegedProcessorTime; } catch { }
+            //try { 获取处理机时长 = eachProcess.UserProcessorTime; } catch { }
+            try { 处理机获取和执行时长 = eachProcess.TotalProcessorTime; } catch { }
+            try { 进程名 = eachProcess.ProcessName; } catch { }
+            try { 正在响应 = eachProcess.Responding; } catch { }
+            try { 启动时间 = eachProcess.StartTime; } catch { }
+            try { 获得虚存大小 = eachProcess.VirtualMemorySize64/1024; } catch { }
+            try { 获得内存大小 = eachProcess.WorkingSet64/1024; } catch { }
+        }
+
+    }
+}
